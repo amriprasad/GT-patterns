f1f1b763e4bf40d1389a8c43da92be8b9d57cafb
Amritanshu Prasad <amri@imsc.res.in>
2017-09-06 08:55:49 +0530

    put in docstrings

--- GTPatterns.py
+++ GTPatterns.py
@@ -1,33 +1,57 @@
 import numpy as np
 import pandas as pd
 
-class GTPatterns:
-	def __init__(self, a):
-		assert all(a[i-1][j] >= a[i][j] >= a[i-1][j+1] for i in range(1, len(a)) for j in range(len(a[i])))
-		self.gt = a
-		self.matrix = pd.DataFrame(a).fillna(0.0).values
-		self.wmatrix = np.zeros(self.matrix.shape)
-		for i in range(len(self.matrix)-1):
-			self.wmatrix[i] = self.matrix[i] - self.matrix[i+1]
-		self.wmatrix[-1] = self.matrix[-1]
-	
-	def word(self):
-		w = ""
-		l = len(self.wmatrix)
-		for j in range(l-1,-1,-1):
-			for i in range(l-j-1,-1,-1):
-				w = w+str(l-i)*int(self.wmatrix[i][j])
-		return(w)
+class GTPattern:
+    def __init__(self, a):
+        """
+        Create a Gelfand-Tsetlin pattern from ``a``.
 
-	def tab(self):
-		w = []
-		l = len(self.wmatrix)
-		for j in range(l):
-			if(np.count_nonzero(self.wmatrix[:,j]) == 0):
-				continue
-			w = w+[[]]
-			for i in range(l-j-1,-1,-1):
-				w[j] = w[j]+[(l-i)]*int(self.wmatrix[i][j])
-		return(w)
+        Example:
+
+            >>> GTPattern([[2, 1, 0], [1, 1], [1]])
+            [[2, 1, 0], [1, 1], [1]]
+        """
+        assert all(a[i-1][j] >= a[i][j] >= a[i-1][j+1] for i in range(1, len(a)) for j in range(len(a[i])))
+        self.gt = a
+        self.matrix = pd.DataFrame(a).fillna(0.0).values
+        self.wmatrix = np.zeros(self.matrix.shape)
+        for i in range(len(self.matrix)-1):
+                self.wmatrix[i] = self.matrix[i] - self.matrix[i+1]
+        self.wmatrix[-1] = self.matrix[-1]
+
+    def __repr__(self):
+        """
+        Return the string representation of ``self``.
+        """
+        return str(self.gt)
+	
+    def word(self):
+        """
+        Return the reading word of the tableaux corresponding to ``self``.
+
+        Assumes that ``self`` is integral.
+        """
+        w = ""
+        l = len(self.wmatrix)
+        for j in range(l-1,-1,-1):
+                for i in range(l-j-1,-1,-1):
+                        w = w+str(l-i)*int(self.wmatrix[i][j])
+        return(w)
+
+    def tab(self):
+        """
+        Return the semistandard tableau corresponding to ``self``.
+
+        Assumes that ``self`` is integral.
+        """
+        w = []
+        l = len(self.wmatrix)
+        for j in range(l):
+            if(np.count_nonzero(self.wmatrix[:,j]) == 0):
+                continue
+            w = w+[[]]
+            for i in range(l-j-1,-1,-1):
+                w[j] = w[j]+[(l-i)]*int(self.wmatrix[i][j])
+            return(w)
 	
 
